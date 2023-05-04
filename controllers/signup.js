a6e2fd13b09dba0593ffcbc8b323c6d9aceaf9d9
diler20289@gmail.com <diler20289@gmail.com>
2023-05-04 10:41:33 +0530

    message from database

--- controllers/signup.js
+++ controllers/signup.js
@@ -2,25 +2,26 @@ const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const sequelize = require('../utility/database');
 const jwt = require('jsonwebtoken');
+const {Op} = require('sequelize');
 
 
 exports.postSignup = async(req,res)=>{
     const t = await sequelize.transaction();
     try{
         const { name, phone, email, password } = req.body;
-        const user = await User.findOne({ where: { email: email }});
+        const user = await User.findOne({ where:{ [Op.or]: [{ email: email }, { phone: phone }] } });
         if(!user){
             bcrypt.hash(password, 10, async(err, hash) =>{
                 if(err){
                     await t.rollback();
                     console.log('err in hash')
                 }
-                const userData = User.create({
+                const userData = await User.create({
                     name: name,
                     phone: phone,
                     email: email,
                     password: hash
-                })
+                },{transaction :t})
                 console.log('userData is ',userData)
                 await t.commit();
                 return res.status(201).json({ userData: userData, message: 'User signup successful' });
