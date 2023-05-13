a46bb868df7c777be5f36145b82b59ac0369e072
raviranjan202 <mailraviranjan28@gmail.com>
2023-05-13 16:26:18 +0530

    mutimedia

--- controllers/chat.js
+++ controllers/chat.js
@@ -67,3 +67,71 @@ exports.getChats = async (req,res) => {
         return res.status(500).json({success: false, error: err})
     }
 }
+
+exports.getMessages=async(req,res,next)=>{
+    try{
+        const lastmsgId=+req.query.lastmsgId||0;
+        const groupId=+req.query.groupId;
+        console.log(lastmsgId);
+        const  messages = await Chat.findAll({
+           where:{groupId:groupId},
+           offset:lastmsgId,
+           limit:10
+        });
+             res.status(200).json({message:messages,success:true})
+    }catch(err){
+        console.log(">>>>>>>>>>>>>>>",err);
+        res.status(500).json({message:"Something went Wrong",success:false})
+    }
+    }
+    
+    
+    function uploadToS3(file){
+    
+        const BUCKET_NAME= process.env.AWS_BUCKET_NAME;;
+        const IAM_USER_KEY= process.env.AWS_KEY_ID;
+        const  IAM_USER_SECRET= process.env.AWS_SECRET_KEY;
+    
+        let s3bucket=new AWS.S3({
+            accessKeyId: IAM_USER_KEY,
+            secretAccessKey:IAM_USER_SECRET,
+        })
+         var params={
+                Bucket:BUCKET_NAME,
+                Key:file.name,
+                Body:file.data,
+                ContentType:file.mimetype,
+                ACL:'public-read'
+            }
+           return new Promise((resolve, reject) => {
+                s3bucket.upload(params,(err,s3response)=>{
+                    if(err){
+                        console.log("SOMETHING WENT WRONG",err)
+                        reject(err);
+                    } 
+                    else{
+                        resolve(s3response.Location)
+                        }
+                    })
+           })      
+    
+    
+    }
+    exports.uploadFile=async(req,res,next)=>{
+        try{
+            const groupId=req.params.groupId;
+    
+            console.log(">>>>>>>",req.files.file);
+            const file=req.files.file;
+            const fileName=file.name;
+            const fileURL= await uploadToS3(file);
+            console.log(fileURL);
+            const user = await req.user.createChat({username:req.user.username,message:fileURL,groupId:groupId});
+            res.status(200).json({message:user,success:true})
+            
+            
+        }catch(err){
+            console.log(">>>>>>>>>>>>>>>",err);
+            res.status(500).json({message:"Something went Wrong",error:err,success:false})
+        }
+    }
