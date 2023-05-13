04bc27be93941df06b9f0884707492d9c4c0968e
raviranjan202 <mailraviranjan28@gmail.com>
2023-05-13 16:22:25 +0530

    scalable

--- frontend/Login/chat.js
+++ frontend/Login/chat.js
@@ -1,168 +1,221 @@
-const token = localStorage.getItem('token');
-const chatForm = document.getElementById('chatForm')
-chatForm.addEventListener('submit',sendMessage);
-const chatDiv = document.getElementById('chatDiv'); 
-let oldMessage = JSON.parse(localStorage.getItem('oldMessage'));
-const msgBox = document.getElementById('msgBox');
-const chatHead = document.getElementById('chatName');
-const header = document.getElementById('header')
-const logout = document.getElementById('logout');
-const chatName = JSON.parse(localStorage.getItem('chatName'));
-const getChat = JSON.stringify({ groupId: chatName.id});
+const chattable=document.getElementById('chattable');
 
-window.addEventListener('DOMContentLoaded',async ()=>{
-    try{
-        chatHead.textContent=chatName.name
-        // if(chatName.isGroup){
-        //     const inviteBtn = document.createElement('button');
-        //     inviteBtn.classList= 'btn btn-right'
-        //     inviteBtn.appendChild(document.createTextNode('Invite Members'))
-        //     header.appendChild(inviteBtn)
-        //     inviteBtn.onclick=(async ()=>{
-        //         const link =  `http://localhost:3000/groups/joinGroup/${chatName.id}`;
-        //         alert(`Share this link with your friends to invite them ${link} `)
-        //     })
-        // }
+const message=document.getElementById('message');
 
-        logout.addEventListener('click',()=>{
-            localStorage.removeItem('token')
-            localStorage.removeItem('oldMessage')
-            window.location.href='./login.html'
-        })
-        if(oldMessage && oldMessage.length>0){
-            
-            showChat(oldMessage);        
-        
-        }
-        else{
-            
-            const response = await axios.get(`http://localhost:3000/chats/getChats?lastMessageId=undefined&&getChat=${getChat}`, {headers: {'Authorization': token}});
-            console.log(response)
-            if(response.data.success){
-                                
-                let chatArray = [...response.data.chatData]
-                chatArray = chatArray.slice(Math.abs(10-response.data.chatData.length))
-                localStorage.setItem('oldMessage',JSON.stringify(chatArray));
-                showChat(chatArray)
+const uploadbtn=document.getElementById('uploadbtn');
 
-                if( response.data.isAdmin){
-                    const inviteBtn = document.createElement('button');
-                    inviteBtn.classList= 'btn btn-right'
-                    inviteBtn.appendChild(document.createTextNode('Invite Members'))
-                    header.appendChild(inviteBtn)
-                    inviteBtn.onclick=(async ()=>{
-                        const link =  `http://localhost:3000/groups/joinGroup/${chatName.id}`;
-                        alert(`Share this link with your friends to invite them ${link} `)
-                    })
-                    const groupControlBtn = document.createElement('button');
-                    groupControlBtn.classList= 'btn btn-right'
-                    groupControlBtn.appendChild(document.createTextNode('group controls'))
-                    header.appendChild(groupControlBtn)
-                    groupControlBtn.onclick=(async ()=>{
-                        window.location.href = './admin.html';
-                    })
-                    // const addMemberBtn = document.createElement('button');
-                    // addMemberBtn.classList= 'btn btn-right'
-                    // addMemberBtn.appendChild(document.createTextNode('Invite Members'))
-                    // header.appendChild(addMemberBtn)
-                    // addMemberBtn.onclick=(async ()=>{
-                    //     const link =  `http://localhost:3000/groups/joinGroup/${chatName.id}`;
-                    //     alert(`Share this link with your friends to invite them ${link} `)
-                    // })
-                    // const removeMemberBtn = document.createElement('button');
-                    // removeMemberBtn.classList= 'btn btn-right'
-                    // removeMemberBtn.appendChild(document.createTextNode('Invite Members'))
-                    // header.appendChild(removeMemberBtn)
-                    // removeMemberBtn.onclick=(async ()=>{
-                    //     const link =  `http://localhost:3000/groups/joinGroup/${chatName.id}`;
-                    //     alert(`Share this link with your friends to invite them ${link} `)
-                    // })
-                }
+const file=document.getElementById('file');
 
-            }
-        }
+const sendbtn=document.getElementById('sendbtn');
 
-            chatDiv.innerHTML='You joined <br>';
-            chatForm.appendChild(chatDiv);
-            setInterval(async () => {
-                let lastMessageId ;
-                oldMessage= JSON.parse(localStorage.getItem('oldMessage'))
-                
-                if(oldMessage &&oldMessage.length>0 ){
-                    lastMessageId = oldMessage[oldMessage.length - 1].id;
-                }
-                
-                const response = await axios.get(`http://localhost:3000/chats/getChats?lastMessageId=${lastMessageId}&&getChat=${getChat}`, {headers: {'Authorization': token}});
-                
-                
-                
-                
-                if (response.data.chatData.length > 0) {
-                    
-                    if(!lastMessageId){
-                        localStorage.setItem('oldMessage', JSON.stringify(response.data.chatData));
-                        showChat(response.data.chatData)
-                        oldMessage = JSON.parse(localStorage.getItem('oldMessage'));
-                        lastMessageId = oldMessage[oldMessage.length - 1].id;
-                    }
-                    const newMessage = response.data.chatData.filter(msg => msg.id > lastMessageId);
+const memberlist=document.getElementById('memberslist');
 
-                    if (newMessage.length > 0) {
-                        
-                        if(oldMessage.length>=10){
-                            const removed= oldMessage.shift()
-                        }
-                        
-                        oldMessage = [...oldMessage, ...newMessage];
-                        localStorage.setItem('oldMessage', JSON.stringify(oldMessage));
-                        chatForm.innerText=''
-                        chatForm.appendChild(msgBox)
-                        showChat(oldMessage);
-                    }
-                }
-                           
-            }, 1000);   
-        
-        }
-        catch(err){
-                console.log(err)
-            }
-        
-        })
+const groupname=document.getElementById('groupname');
 
-function showChat(arr){
-    for(let i=0 ;i<arr.length;i++){     
-    chatForm.innerHTML = chatForm.innerHTML+`${arr[i].username}: ${arr[i].message} <br>`;
-    }
-}
-async function sendMessage(e){
+const deletegroupbtn=document.getElementById('deletegroupbtn');
+
+const adminname=document.getElementById('adminname')
+
+sendbtn.addEventListener('click',sendmessage);
+
+async function sendmessage(e){
     try{
         e.preventDefault();
-        let obj
-        let message = document.getElementById('message')
-        if(chatName.isGroup){
-            obj={
-                message:message.value,
-                groupId: chatName.id,
-                
-            }
-        }else{
-            obj ={
-                message:message.value,
+           if(message.value==='' )
+           {
+               msg.innerHTML="Please Enter message";
+               setTimeout(()=>{
+                   msg.innerHTML="";
+               },3000)
+           }
+           else{
+               const messagedata={
+                  message:message.value
+               }
+               const token=localStorage.getItem('token');
+               const groupId=JSON.parse(localStorage.getItem('groupId'));
+            
+               const response= await axios.post(`http://localhost:3000/chat/add-message/${groupId}`,messagedata,{headers:{"Authorization":token}})
 
-                groupId: null
-            }
-        }
-        
-        let sendMessage = await axios.post("http://localhost:3000/chats/sendMessage",obj, {headers: {'Authorization': token}});
-        console.log(sendMessage)
-        if(sendMessage.data.success){
-            // chatForm.innerHTML = chatForm.innerHTML + `You: ${message.value} <br>`;
-            console.log(chatDiv)
-            message.value = '';
-        }
-
-    }catch(err){
-        console.log('ERR Send_Message',err)
+               console.log(response.data,">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+               
+                    showmessage(response.data.message.username,response.data.message.message)
+                    message.value='';
+           }
+       }
+       catch(err){
+           console.log(err);
+           msg.innerHTML="";
+         msg.innerHTML=msg.innerHTML+`<div>${err.response.data.message}</div>`;
+         setTimeout(()=>{
+           msg.innerHTML="";
+       },3000)
+       }
     }
+
+    //decode token
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
 }
+    async function showmessage(username,message){
+        try{
+            const token=localStorage.getItem('token');
+            const decodedtoken=parseJwt(token);
+            let className;
+            if(username===decodedtoken.name)
+            {
+              className='currentuser'
+            }
+            else{
+                className-'otheruser'
+            }
+            chattable.className="tbody";
+            chattable.innerHTML+=`<tr class=${className}><td>${username}-${message}</td></tr>`;
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
+    window.addEventListener('DOMContentLoaded',getchats);
+    window.addEventListener('DOMContentLoaded',getgroupmembers);
+
+    async function getchats(){
+        try{
+
+                   groupname.innerHTML=JSON.parse(localStorage.getItem('groupName'));
+
+                   let groupId=JSON.parse(localStorage.getItem('groupId'));
+                   let lastmsgId=JSON.parse(localStorage.getItem(`lastmsgId${groupId}`));
+                   
+
+                   console.log(lastmsgId,">>>>>>>>>>>>")
+                   const response= await axios.get(`http://localhost:3000/chat/get-message?lastmsgId=${lastmsgId}&groupId=${groupId}`);
+                   console.log(response,">>>>>>>>>>>>>")
+                   lastmsgId+=parseInt(response.data.message.length);
+                   console.log(lastmsgId,">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+                  
+                   let existingArray=JSON.parse(localStorage.getItem(`messages${groupId}`))||[];
+
+                   if(existingArray.length>=10){
+                    existingArray=[];
+                   }
+
+                   let responseArray=response.data.message;
+
+                   let mergedArray=existingArray.concat(responseArray);
+
+                   localStorage.setItem(`lastmsgId${groupId}`,JSON.stringify(lastmsgId));
+
+                   localStorage.setItem(`messages${groupId}`,JSON.stringify(mergedArray));
+
+                   chattable.innerHTML='';
+
+                   for(let i=0;i<mergedArray.length;i++){
+                        showmessage(mergedArray[i].username,mergedArray[i].message)
+                   }
+                   chattable.scrollTop=chattable.scrollHeight;
+                  
+           }
+           catch(err){
+               console.log(err);
+               msg.innerHTML="";
+             msg.innerHTML=msg.innerHTML+`<div>${err.response.data.message}</div>`;
+             setTimeout(()=>{
+               msg.innerHTML="";
+           },3000)
+           }
+        }
+
+        /*  setInterval(()=>{
+            getchats();
+            getgroupmembers();
+        },1000);   */
+
+        async function getgroupmembers(){
+            try{
+
+                const groupId=JSON.parse(localStorage.getItem('groupId'));
+                const response= await axios.get(`http://localhost:3000/group/get-members?groupId=${groupId}`);
+                console.log(response);
+                adminname.innerHTML=`Admin-${response.data.username.username}`;
+                memberlist.innerHTML='';
+                for(let i=0;i<response.data.message.length;i++){
+                    memberlist.innerHTML+=`<li>${response.data.message[i].username}</li>`;
+                }
+
+            }catch(err){
+                console.log(err);
+                msg.innerHTML="";
+              msg.innerHTML=msg.innerHTML+`<div>${err.response.data.message}</div>`;
+              setTimeout(()=>{
+                msg.innerHTML="";
+            },3000)
+            }
+        }
+
+        deletegroupbtn.addEventListener('click',deleteGroup)
+   
+       async function deleteGroup(){
+        try{
+            const groupId=JSON.parse(localStorage.getItem('groupId'));
+            const token=localStorage.getItem('token');
+            const response= await axios.delete(`http://localhost:3000/group/delete-group?groupId=${groupId}`,{headers:{"Authorization":token}});
+            alert(response.data.message);
+            localStorage.removeItem(`messages${groupId}`);
+            localStorage.removeItem(`lastmsgId${groupId}`);
+            localStorage.removeItem('groupName');
+            localStorage.removeItem('groupId');
+            window.location.href='../Group/group.html';
+
+            
+        }catch(err){
+            console.log(err);
+            msg.innerHTML="";
+          msg.innerHTML=msg.innerHTML+`<div>${err.response.data.message}</div>`;
+          setTimeout(()=>{
+            msg.innerHTML="";
+        },3000)
+        }
+       }
+
+       uploadbtn.addEventListener('click',uploadFile);
+
+       async function uploadFile(e){
+        try{
+            e.preventDefault();
+            const uploadedfile=file.files[0];
+            console.log(uploadedfile);
+            if(!uploadedfile){
+               msg.innerHTML="Please Upload a file ";
+               setTimeout(()=>{
+                   msg.innerHTML="";
+               },3000)
+           }
+           else{
+            const formData=new FormData();
+            formData.append('file',uploadedfile);
+            console.log(formData);
+            const groupId=JSON.parse(localStorage.getItem('groupId'));
+            const token=localStorage.getItem('token');
+            const response=await axios.post(`http://localhost:3000/chat/sendfile/${groupId}`,formData,{headers:{"Authorization":token,'Content-Type':'multipart/form-data'}});
+                console.log(response);
+                showmessage(response.data.message.username,response.data.message.message)
+                uploadedfile.value=null;
+           }
+        }catch(err){
+            console.log(err);
+            msg.innerHTML="";
+          msg.innerHTML=msg.innerHTML+`<div>${err.response.data.message}</div>`;
+          setTimeout(()=>{
+            msg.innerHTML="";
+        },3000)
+        }
+       }
